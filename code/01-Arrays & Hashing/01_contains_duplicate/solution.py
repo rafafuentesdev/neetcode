02391a383e584a067b6f068ab6f919c7d6871ccc
rafafuentesdev <github@rafafuentesdev.com>
2024-08-10 02:46:37 +0200

    add complete solution

--- code/01-Arrays & Hashing/01_contains_duplicate/solution.py
+++ code/01-Arrays & Hashing/01_contains_duplicate/solution.py
@@ -1,6 +1,7 @@
 from typing import List
 
-class Solution:
+
+class BruteForceSolution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
@@ -9,3 +10,26 @@ class Solution:
         return False
 
 
+class SortingSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+
+
+class HashSetSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
+
+
+class ConciseSetSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
