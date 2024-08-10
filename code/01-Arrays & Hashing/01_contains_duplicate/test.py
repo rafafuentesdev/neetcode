4f108486e04c6bc7285f73ea29ea5d7375a530be
rafafuentesdev <github@rafafuentesdev.com>
2024-08-10 02:46:56 +0200

    add complete tests to the solution

--- code/01-Arrays & Hashing/01_contains_duplicate/test.py
+++ code/01-Arrays & Hashing/01_contains_duplicate/test.py
@@ -1,35 +1,28 @@
 import pytest
-from solution import Solution
+from solution import (
+    BruteForceSolution,
+    SortingSolution,
+    HashSetSolution,
+    ConciseSetSolution,
+)
 
 
-@pytest.fixture
-def solution():
-    return Solution()
+@pytest.fixture(
+    params=[BruteForceSolution, SortingSolution, HashSetSolution, ConciseSetSolution]
+)
+def solution(request):
+    return request.param()
 
 
-def test_contains_duplicate_example1(solution):
-    assert solution.containsDuplicate([1, 2, 3, 3]) == True
-
-
-def test_contains_duplicate_example2(solution):
-    assert solution.containsDuplicate([1, 2, 3, 4]) == False
-
-
-def test_contains_duplicate_empty_list(solution):
-    assert solution.containsDuplicate([]) == False
-
-
-def test_contains_duplicate_single_element(solution):
-    assert solution.containsDuplicate([1]) == False
-
-
-def test_contains_duplicate_all_same(solution):
-    assert solution.containsDuplicate([2, 2, 2, 2]) == True
-
-
-def test_contains_duplicate_negative_numbers(solution):
-    assert solution.containsDuplicate([-1, -2, -3, -1]) == True
-
-
-def test_contains_duplicate_mixed_numbers(solution):
-    assert solution.containsDuplicate([1, -1, 2, -2, 3, -3]) == False
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        ([1, 2, 3, 3], True),
+        ([1, 2, 3, 4], False),
+        ([1], False),
+        ([], False),
+        ([1, 1, 1, 1], True),
+    ],
+)
+def test_contains_duplicate(solution, input_data, expected):
+    assert solution.containsDuplicate(input_data) == expected
